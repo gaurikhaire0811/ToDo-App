cd12796ad88af137ae746afb17e44e91abe5aff7
Gauri Khaire <gaurikhaire191@gmail.com>
2026-05-20 01:41:42 +0530

    fixed router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
 
 // npm install react-router-dom
+// import './App.css';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import TodoList from "./TodoList";
+// import AddTaskPage from "./AddTaskPage";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<TodoList />} />
+//         <Route path="/add" element={<AddTaskPage />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TodoList from "./TodoList";
@@ -7,7 +25,7 @@ import AddTaskPage from "./AddTaskPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/ToDo-App">
       <Routes>
         <Route path="/" element={<TodoList />} />
         <Route path="/add" element={<AddTaskPage />} />
